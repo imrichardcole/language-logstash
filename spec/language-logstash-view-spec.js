e7e467f690c502414275787c4fc47379dad02ccc
Richard Cole <richandkerrycole@gmail.com>
2016-04-16 08:38:09 +0100

    initial commit

---
+++ spec/language-logstash-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageLogstashView from '../lib/language-logstash-view';
+
+describe('LanguageLogstashView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
